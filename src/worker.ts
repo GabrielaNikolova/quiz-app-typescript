f45873b181cf83ec13876ca24c471adaba1c77e6
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2023-12-13 13:50:54 +0200

    fix(worker.ts): fix error

--- src/worker.ts
+++ src/worker.ts
@@ -2,11 +2,16 @@ import { BlobWriter, TextReader, ZipWriter } from "@zip.js/zip.js";
 
 onmessage = async (e) => {
     const { feedback } = e.data;
+    if (typeof feedback != 'string') {
+        return;
+    } else {
+        feedback.replace(/<br\/>/g, '\n');
 
-    feedback.replaceAll('<br/>', '\n');
+        const zipWriter = new ZipWriter(new BlobWriter("application/zip"));
+        await zipWriter.add("QuizResult.txt", new TextReader(feedback));
+        const blob = await zipWriter.close();
+        postMessage(blob)
+    };
 
-    const zipWriter = new ZipWriter(new BlobWriter("application/zip"));
-    await zipWriter.add("QuizResult.txt", new TextReader(feedback));
-    const blob = await zipWriter.close()
-    postMessage(blob)
+
 };
