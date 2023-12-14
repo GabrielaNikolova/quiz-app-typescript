462b5f0199734e167587582a2ba20119b3b9196b
GabrielaNikolova <62817091+GabrielaNikolova@users.noreply.github.com>
2023-12-14 10:40:19 +0200

    refactor(webpack.config.js): change configuration

--- webpack.config.js
+++ webpack.config.js
@@ -10,13 +10,6 @@ module.exports = [
     {
         mode: 'development',
         entry: './src/app.ts',
-        devServer: {
-            static: [
-                {
-                    directory: path.join(__dirname),
-                },
-            ],
-        },
         output: {
             filename: 'bundle.js',
             path: path.resolve(__dirname, 'dist'),
